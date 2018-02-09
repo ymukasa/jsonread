dc7fbae47c923ee67ebf3396cc4b87af7aac1c18
mukasa <ball.head.21@gmail.com>
2018-02-09 15:53:20 +0900

    function の整理

--- src/jsonread.js
+++ src/jsonread.js
@@ -1,16 +1,43 @@
 const fs = require('fs');
-
 const remote = require('electron').remote;
 const dialog = remote.dialog;
 
 /**
  * JSONファイルの読み込みを行う。
  */
-function read() {
+function createList() {
+    var createList = (filePath, fileName, fileContents) => {
+        var jsonData = JSON.parse(fileContents);
+        var tbody = document.getElementById("resultListBody");
+        var tr = tbody.insertRow(-1);
+        var td1 = tr.insertCell(-1);
+        var td2 = tr.insertCell(-1);
+        var td3 = tr.insertCell(-1);
+        var td4 = tr.insertCell(-1);
+        td1.innerHTML = fileName;
+        td2.innerHTML = jsonData.id;
+        td3.innerHTML = jsonData.name;
+        td4.innerHTML = jsonData.physicalName;
+    };
+    clearTable();
+    readJson(createList);
+}
+
+/** 
+ * テーブルの一覧をクリアする。
+ */
+function clearTable() {
     var tbody = document.getElementById("resultListBody");
     while( tbody.rows[0] ) {
         tbody.deleteRow( 0 );
     }
+}
+
+/**
+ * 指定されたディレクトリ配下のjsonファイルを読む。
+ * @param {function} callbackFileRead 読んだjsonファイル１つ分の内容を処理する関数。
+ */
+function readJson(callbackFileRead) {
     var dirPath = document.getElementById("dirpath").value;
     if (!dirPath.endsWith("/") && !dirPath.endsWith("\\")) {
         dirPath += "\\";
@@ -24,16 +51,7 @@ function read() {
                     if (err) throw err;
                     fs.readFile(filePath, "utf8", function(err, fileContents) {
                         if (err) throw err;
-                        var jsonData = JSON.parse(fileContents);
-                        var tr = tbody.insertRow(-1);
-                        var td1 = tr.insertCell(-1);
-                        var td2 = tr.insertCell(-1);
-                        var td3 = tr.insertCell(-1);
-                        var td4 = tr.insertCell(-1);
-                        td1.innerHTML = fileName;
-                        td2.innerHTML = jsonData.id;
-                        td3.innerHTML = jsonData.name;
-                        td4.innerHTML = jsonData.physicalName;
+                        callbackFileRead(filePath, fileName, fileContents);
                     });
                 });
             }
@@ -50,6 +68,8 @@ function selectDirectory() {
         title: 'フォルダ(単独選択)',
         defaultPath: '.'
     }, (folderNames) => {
-        document.getElementById("dirpath").value = folderNames[0];
+        if (folderNames !== undefined && folderNames.length > 0) {
+            document.getElementById("dirpath").value = folderNames[0];
+        }
     });
 }
